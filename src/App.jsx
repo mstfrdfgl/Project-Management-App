86c5958c7cb8c35f8db4025173d4b30b8773fcfe
Mustafa Redifoglu <mustafaredifoglu@gmail.com>
2024-06-16 17:01:51 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ProjectsSidebar from "../components/ProjectsSidebar";
+function App() {
+  con;
+  return (
+    <>
+      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ProjectsSidebar.jsx
@@ -0,0 +1,7 @@
+
+export default function ProjectsSidebar() {
+  return (
+    <div>ProjectsSidebar</div>
+    
+  )
+}
